ec823ad9f724499139f478a522edaa36f97b3da4
Hurricaaane <git@ha3.eu>
2014-01-03 19:03:33 +0100

    Removed License note

--- pf-src/eu/ha3/mc/presencefootsteps/mcpackage/implem/AcousticsManager.java
+++ pf-src/eu/ha3/mc/presencefootsteps/mcpackage/implem/AcousticsManager.java
@@ -17,21 +17,7 @@ import eu.ha3.mc.presencefootsteps.engine.interfaces.SoundPlayer;
 import eu.ha3.mc.presencefootsteps.mcpackage.interfaces.DefaultStepPlayer;
 import eu.ha3.mc.presencefootsteps.mcpackage.interfaces.Isolator;
 
-/*
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-                    Version 2, December 2004 
-
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
-
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this license document, and changing it is allowed as long 
- as the name is changed. 
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
-*/
+/* x-placeholder-wtfplv2 */
 
 /**
  * A Library that can also play sounds and default footsteps.
