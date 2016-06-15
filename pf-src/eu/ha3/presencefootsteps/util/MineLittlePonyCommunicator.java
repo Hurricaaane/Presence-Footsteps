12b521614026ed3c7b287456b0955e197f6670cf
Sollace <mwchris@mweb.co.za>
2016-06-15 14:59:35 +0200

    Update MineLP compatibility

--- pf-src/eu/ha3/presencefootsteps/util/MineLittlePonyCommunicator.java
+++ pf-src/eu/ha3/presencefootsteps/util/MineLittlePonyCommunicator.java
@@ -1,25 +1,27 @@
 package eu.ha3.presencefootsteps.util;
 
-import com.minelittlepony.minelp.Pony;
-import com.minelittlepony.minelp.PonyLevel;
-import com.minelittlepony.minelp.PonyManager;
+import com.brohoof.minelittlepony.MineLittlePony;
+import com.brohoof.minelittlepony.Pony;
+import com.brohoof.minelittlepony.PonyLevel;
+
 import com.sollace.unicopia.Race;
 import com.sollace.unicopia.server.PlayerSpeciesRegister;
 
 import eu.ha3.mc.convenience.Ha3StaticUtilities;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.entity.player.EntityPlayer;
 
 public class MineLittlePonyCommunicator {
 	public static final MineLittlePonyCommunicator instance = new MineLittlePonyCommunicator();
 	
 	private static boolean unicopiaDetected = Ha3StaticUtilities.classExists("com.sollace.unicopia.Race", instance);
-	private static boolean MineLPDetected = Ha3StaticUtilities.classExists("com.minelittlepony.minelp.Pony", instance);
+	private static boolean MineLPDetected = Ha3StaticUtilities.classExists("com.brohoof.minelittlepony.LiteModMineLittlePony", instance);
 	
 	private boolean arePoniesShown() {
 		if (!MineLPDetected) return false;
 		try {
-			PonyLevel level = PonyManager.getInstance().getPonyLevel();
-			return level == PonyLevel.PONIES || level == PonyLevel.MIXED;
+			PonyLevel level = MineLittlePony.getInstance().getConfig().getPonyLevel();
+			return level == PonyLevel.PONIES || level == PonyLevel.BOTH;
 		} catch (Throwable e) {
 			e.printStackTrace();
 			return MineLPDetected = false;
@@ -43,8 +45,8 @@ public class MineLittlePonyCommunicator {
 			}
 		}
 		//get type of stance (4 legged / pegasus) if MineLP is installed
-		if (arePoniesShown()) {
-			Pony pony = PonyManager.getInstance().getPonyFromResourceRegistry(ply);
+		if (arePoniesShown() && ply instanceof AbstractClientPlayer) {
+			Pony pony = MineLittlePony.getInstance().getManager().getPonyFromResourceRegistry((AbstractClientPlayer)ply);
 			return pony.isPegasus() ? 2 : 1;
 		}
 		return 0;
