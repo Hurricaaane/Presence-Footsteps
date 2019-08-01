bec1087f2bf76e0db994950a3aa5015ae8611e1c
Sollace <mwchris@mweb.co.za>
2019-08-01 13:22:57 +0200

    Moved PendingSound into DelayedSoundPlayer

--- src/main/java/eu/ha3/presencefootsteps/sound/player/DelayedSoundPlayer.java
+++ src/main/java/eu/ha3/presencefootsteps/sound/player/DelayedSoundPlayer.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import eu.ha3.presencefootsteps.PresenceFootsteps;
 import eu.ha3.presencefootsteps.sound.Options;
+import eu.ha3.presencefootsteps.util.MathUtil;
 import net.minecraft.entity.Entity;
 
 class DelayedSoundPlayer implements SoundPlayer {
+
+    private static final boolean USING_LATENESS = true;
+    private static final boolean USING_EARLYNESS = true;
+
+    private static final float LATENESS_THRESHOLD = 1.5F;
+
+    private static final double EARLYNESS_THRESHOLD_POW = 0.75D;
 
     private final List<PendingSound> pending = new ArrayList<>();
 
@@ -27,7 +36,7 @@ class DelayedSoundPlayer implements SoundPlayer {
 
     @Override
     public void playSound(Entity location, String soundName, float volume, float pitch, Options options) {
-        pending.add(new PendingSound(location, soundName, getRNG(), volume, pitch, options, nextPlayTime));
+        pending.add(new PendingSound(location, soundName, volume, pitch, options));
     }
 
     @Override
@@ -40,19 +49,73 @@ class DelayedSoundPlayer implements SoundPlayer {
 
         nextPlayTime = Long.MAX_VALUE;
 
-        pending.removeIf(this::tickPendingSound);
+        pending.removeIf(PendingSound::tick);
     }
 
-    private boolean tickPendingSound(PendingSound sound) {
-        switch (sound.nextState(currentTime)) {
-        case PLAYING:
-            sound.play(immediate);
-            return false;
-        case SKIPPING:
-            return true;
-        default:
-            nextPlayTime = sound.getTimeToPlay();
-            return false;
+    private class PendingSound {
+        private final Entity location;
+
+        private final String soundName;
+
+        private final float volume;
+        private final float pitch;
+
+        private final long timeToPlay;
+        private final long maximum;
+
+        public PendingSound(Entity location, String soundName, float volume, float pitch, Options options) {
+            this.location = location;
+            this.soundName = soundName;
+            this.volume = volume;
+            this.pitch = pitch;
+
+            maximum = options.containsKey("skippable") ? -1L : options.get("delay_max");
+            timeToPlay = System.currentTimeMillis() + Math.max(MathUtil.randAB(getRNG(),
+                    options.get("delay_min"),
+                    options.get("delay_max")
+            ), nextPlayTime);
         }
+
+        public boolean tick() {
+            switch (nextState(currentTime)) {
+            case PLAYING:
+                immediate.playSound(location, soundName, volume, pitch, Options.EMPTY);
+                return false;
+            case SKIPPING:
+                return true;
+            default:
+                nextPlayTime = timeToPlay;
+                return false;
+            }
+        }
+
+        private State nextState(long time) {
+            if (time >= timeToPlay || USING_EARLYNESS
+             && time >= timeToPlay - Math.pow(maximum, EARLYNESS_THRESHOLD_POW)) {
+                if (USING_EARLYNESS && time < timeToPlay) {
+                    PresenceFootsteps.logger.debug("Playing early sound (early by " + (timeToPlay - time) + "ms, tolerence is " + Math.pow(maximum, EARLYNESS_THRESHOLD_POW));
+                }
+
+                long lateness = time - timeToPlay;
+
+                if (!USING_LATENESS
+                        || maximum < 0
+                        || lateness <= maximum / LATENESS_THRESHOLD) {
+                    return State.PLAYING;
+                }
+
+                PresenceFootsteps.logger.debug("Skipped late sound (late by " + lateness + "ms, tolerence is " + maximum / LATENESS_THRESHOLD + "ms)");
+
+                return State.SKIPPING;
+            }
+
+            return State.PENDING;
+        }
+    }
+
+    enum State {
+        PENDING,
+        PLAYING,
+        SKIPPING
     }
 }
