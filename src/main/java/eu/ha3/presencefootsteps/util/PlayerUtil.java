9809f87c91ffdb827ea84357ec9fe7fb3e01d3bc
Sollace <sollacea@gmail.com>
2020-04-06 15:57:38 +0200

    Fixed footsteps spam when other players are nearby

--- src/main/java/eu/ha3/presencefootsteps/util/PlayerUtil.java
+++ src/main/java/eu/ha3/presencefootsteps/util/PlayerUtil.java
@@ -1,25 +1,16 @@
 package eu.ha3.presencefootsteps.util;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 
 public class PlayerUtil {
     public static boolean isClientPlayer(Entity entity) {
-        return resolveToClientPlayer(entity) == entity;
-    }
-
-    public static PlayerEntity resolveToClientPlayer(Entity entity) {
         PlayerEntity client = MinecraftClient.getInstance().player;
-
-        if (entity instanceof PlayerEntity) {
-            if (client != null && (client == entity || client.getUuid().equals(entity.getUuid()))) {
-                return client;
-            }
-
-            return (PlayerEntity)entity;
-        }
-
-        return client;
+        return entity instanceof PlayerEntity
+                && !(entity instanceof OtherClientPlayerEntity)
+                && client != null
+                && (client == entity || client.getUuid().equals(entity.getUuid()));
     }
 }
